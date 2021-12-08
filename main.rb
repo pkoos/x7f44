c3bde4593985fd8a4208ba63947fe80ef37071b8
Paul Koos <paully_k5298@yahoo.com>
2021-12-08 12:14:14 -0800

    format main file by removing extra white space and indentation.

--- main.rb
+++ main.rb
@@ -1,23 +1,16 @@
 require_relative 'game'
-  require_relative 'box'
-  require_relative 'die'
-
-
+require_relative 'box'
+require_relative 'die'
 
 NUM_T = 9
 N_DICE = 2
 N_SIDES = 6
-
 
 dice= (1..N_DICE).map { |i| Die.new(N_SIDES, Random.new) }
 box= Box.new(NUM_T)
 box.set_tiles
 
 game=Game.new(box, dice)
-
-
-
-
 
 puts Game::WELCOME_MESSAGE
  game.start!
